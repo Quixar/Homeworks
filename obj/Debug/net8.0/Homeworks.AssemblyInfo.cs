3062e00636614ba4364a8b7e97812d1cf99e490e
Quixar <evgenmart01@gmail.com>
2024-09-18 16:25:29 +0300

    first commit

--- obj/Debug/net8.0/Homeworks.AssemblyInfo.cs
+++ obj/Debug/net8.0/Homeworks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homeworks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cee8a1cb75aee74a3aad60ef3536f3aa5c5e294")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e314fe824514f6e3316194a8e09a4c96381f28b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homeworks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homeworks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

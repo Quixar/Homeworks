503d0fd4d1755061b39524570b5d692bb856ff07
Quixar <evgenmart01@gmail.com>
2024-10-08 15:59:37 +0300

    hw done

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Headers;
-using System.Security.Cryptography.X509Certificates;
 
 namespace student;
 
--- obj/Debug/net8.0/Homeworks.AssemblyInfo.cs
+++ obj/Debug/net8.0/Homeworks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homeworks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06efa0b535f21c61285f900d418a415432928e74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bd1c783eab62b0d930502a3706f738998534637")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homeworks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homeworks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

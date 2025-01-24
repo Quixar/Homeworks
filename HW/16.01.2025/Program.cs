8d1f4daa1b0ae972d716105bddd053aa082a691a
Quixar <evgenmart01@gmail.com>
2025-01-24 16:29:32 +0200

    hw 16.01.2025 done

---
+++ HW/16.01.2025/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            string app = "terminal";
+
+            Process process = new Process();
+            process.StartInfo.FileName = "open";
+            process.StartInfo.Arguments = $"-a {app}";
+
+            process.Start();
+
+            process.WaitForExit();
+
+            int exitCode = process.ExitCode;
+            System.Console.WriteLine($"Process has been ended. Exit code {exitCode}");
+        }
+        catch (Exception ex)
+        {
+            System.Console.WriteLine($"Error {ex.Message}");
+        }
+
+    }
+}

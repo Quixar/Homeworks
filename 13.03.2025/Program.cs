05444584ff7500905707445e89a3df45f6593795
Quixar <evgenmart01@gmail.com>
2025-04-27 00:22:56 +0300

    HW done

---
+++ 13.03.2025/Program.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        MySqlConnection connection =
+            new MySqlConnection("server=localhost;port=3306;database=FruitsAndVegetables;uid=root;password=INnoVation");
+        try
+        {
+            connection.Open();
+            Console.WriteLine("Connection established");
+            Console.WriteLine($"BD {connection.Database}");
+        }
+        catch
+        {
+            Console.WriteLine("Connection Failed");
+        }
+        finally
+        {
+            connection.Close();
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}

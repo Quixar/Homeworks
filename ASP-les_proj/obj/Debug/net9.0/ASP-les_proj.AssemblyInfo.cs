c68e2df383125add3c3e4775fddc2ae7c5c57102
Quixar <evgenmart01@gmail.com>
2025-07-02 08:34:00 +0300

    hw done

--- ASP-les_proj/obj/Debug/net9.0/ASP-les_proj.AssemblyInfo.cs
+++ ASP-les_proj/obj/Debug/net9.0/ASP-les_proj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP-les_proj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43422a39017c287e6bcc962fec644d4091a02904")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP-les_proj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP-les_proj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
